4146f5841fe2e281bcf6ed85f3bcae88cd5f51f0
Kris Wilson <kwilson@twitter.com>
2015-07-26 17:04:34 -0700

    Drop writer lock for queue + writer. Update docs.

--- README.md
+++ README.md
@@ -5,14 +5,14 @@ a python trace profiler that outputs to chrome trace-viewer format (about://trac
 
     from pytracing import TraceProfiler
     tp = TraceProfiler(output=open('/tmp/trace.out', 'wb'))
-    tp.install()
-    ...
-    tp.shutdown()
-
-
-  alternatively, using the context manager:
-
-    from pytracing import TraceProfiler
-    tp = TraceProfiler(output=open('/tmp/trace.out', 'wb'))
     with tp.traced():
-      ...
+      ...execute something here...
+
+# screenshots
+
+![click to view detail](http://kwlzn.github.io/img/pytracing-screen-1.png "click to view detail")
+
+![selection and aggregate view](http://kwlzn.github.io/img/pytracing-screen-2.png "selection and aggregate view")
+
+![zoom](http://kwlzn.github.io/img/pytracing-screen-3.png "zoom")
+
--- pytracing/pytracing.py
+++ pytracing/pytracing.py
@@ -1,13 +1,27 @@
 from contextlib import contextmanager
+from Queue import Queue
 import json
 import os
 import sys
 import threading
 import time
 
-
 def to_microseconds(s):
   return 1000000 * float(s)
+
+
+class TraceWriter(threading.Thread):
+  def __init__(self, terminator, input_queue, output_stream):
+    threading.Thread.__init__(self)
+    self.daemon = True
+    self.terminator = terminator
+    self.input = input_queue
+    self.output = output_stream
+
+  def run(self):
+    while not self.terminator.is_set():
+      item = self.input.get()
+      self.output.write(item)
 
 
 class TraceProfiler(object):
@@ -17,18 +31,19 @@ class TraceProfiler(object):
 
         from pytracing import TraceProfiler
         tp = TraceProfiler(output=open('/tmp/trace.out', 'wb'))
-        tp.install()
-        ...
-        tp.shutdown()
+        with tp.traced():
+          ...
 
   """
   TYPES = {'call': 'B', 'return': 'E'}
-  WRITE_LOCK = threading.Lock()
 
   def __init__(self, output, clock=None):
-    self._output = output
+    self.output = output
     self.clock = clock or time.time
     self.pid = os.getpid()
+    self.queue = Queue()
+    self.terminator = threading.Event()
+    self.writer = TraceWriter(self.terminator, self.queue, self.output)
 
   @property
   def thread_id(self):
@@ -38,27 +53,32 @@ class TraceProfiler(object):
   def traced(self):
     """Context manager for install/shutdown in a with block."""
     self.install()
-    yield
-    self.shutdown()
+    try:
+      yield
+    finally:
+      self.shutdown()
 
   def install(self):
     """Install the trace function and open the JSON output stream."""
-    self._open_collection()
-    sys.setprofile(self.tracer)
-    threading.setprofile(self.tracer)
+    self._open_collection()            # Open the JSON output.
+    self.writer.start()                # Start the writer thread.
+    sys.setprofile(self.tracer)        # Set the trace/profile function.
+    threading.setprofile(self.tracer)  # Set the trace/profile function for threads.
 
   def shutdown(self):
-    sys.setprofile(None)
-    threading.setprofile(None)
-    self._close_collection()
+    sys.setprofile(None)               # Clear the trace/profile function.
+    threading.setprofile(None)         # Clear the trace/profile function for threads.
+    self._close_collection()           # Close the JSON output.
+    self.terminator.set()              # Stop the writer thread.
+    self.writer.join()                 # Join the writer thread.
 
   def _open_collection(self):
     """Write the opening of a JSON array to the output."""
-    self._output.write('[\n')
+    self.queue.put('[\n')
 
   def _close_collection(self):
     """Write the closing of a JSON array to the output."""
-    self._output.write('{}\n]\n')
+    self.queue.put('{}\n]\n')
 
   def fire_event(self, event_type, func_name, func_filename, func_line_no,
                  caller_filename, caller_line_no):
@@ -78,19 +98,20 @@ class TraceProfiler(object):
                   caller=':'.join([str(x) for x in (caller_filename, caller_line_no)]),
                 )
               )
-            )
-
-    with self.WRITE_LOCK:
-      self._output.write(event + ',\n')
+            ) + ',\n'
+    self.queue.put(event)
 
   def tracer(self, frame, event_type, arg):
     """Bound tracer function for sys.settrace()."""
-    if event_type in self.TYPES.keys() and frame.f_code.co_name != 'write':
-      self.fire_event(
-        event_type=event_type,
-        func_name=frame.f_code.co_name,
-        func_filename=frame.f_code.co_filename,
-        func_line_no=frame.f_lineno,
-        caller_filename=frame.f_back.f_code.co_filename,
-        caller_line_no=frame.f_back.f_lineno,
-      )
+    try:
+      if event_type in self.TYPES.keys() and frame.f_code.co_name != 'write':
+        self.fire_event(
+          event_type=event_type,
+          func_name=frame.f_code.co_name,
+          func_filename=frame.f_code.co_filename,
+          func_line_no=frame.f_lineno,
+          caller_filename=frame.f_back.f_code.co_filename,
+          caller_line_no=frame.f_back.f_lineno,
+        )
+    except Exception:
+      pass     # Don't disturb execution if we can't log the trace.
