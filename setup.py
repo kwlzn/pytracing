422ce3bc87e90db693425de34962949d71a657ff
Kris Wilson <kwilson@twitter.com>
2019-02-14 12:56:45 -0800

    Release 0.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
   name='pytracing',
-  version='0.2',
+  version='0.3',
   description='a python trace profiler that outputs to chrome trace-viewer format (about://tracing).',
   author='Kris Wilson',
   author_email='kwilson@twitter.com',
