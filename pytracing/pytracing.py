b6c3267f7a01a9e11605e6327e15866ffbba855b
Pedro Larroy <pedro.larroy.lists@gmail.com>
2021-09-16 05:18:54 -0700

    Add GH actions CI, lint and test.

--- pytracing/pytracing.py
+++ pytracing/pytracing.py
@@ -15,119 +15,118 @@ from contextlib import contextmanager
 logger = logging.getLogger(__name__)
 
 try:
-  from queue import Queue
+    from queue import Queue
 except ImportError:
-  from Queue import Queue
+    from Queue import Queue
 
 
 def to_microseconds(s):
-  return 1000000 * float(s)
+    return 1000000 * float(s)
 
 
 class TraceWriter(threading.Thread):
+    def __init__(self, terminator, input_queue, output_stream):
+        threading.Thread.__init__(self)
+        self.daemon = True
+        self.terminator = terminator
+        self.input = input_queue
+        self.output = output_stream
 
-  def __init__(self, terminator, input_queue, output_stream):
-    threading.Thread.__init__(self)
-    self.daemon = True
-    self.terminator = terminator
-    self.input = input_queue
-    self.output = output_stream
+    def _open_collection(self):
+        """Write the opening of a JSON array to the output."""
+        logger.debug("_open_collection")
+        self.output.write("[")
 
-  def _open_collection(self):
-    """Write the opening of a JSON array to the output."""
-    logger.debug('_open_collection')
-    self.output.write('[')
+    def _close_collection(self):
+        """Write the closing of a JSON array to the output."""
+        logger.debug("_close_collection")
+        self.output.write("{}]")  # empty {} so the final entry doesn't end with a comma
 
-  def _close_collection(self):
-    """Write the closing of a JSON array to the output."""
-    logger.debug('_close_collection')
-    self.output.write('{}]')  # empty {} so the final entry doesn't end with a comma
-
-  def run(self):
-    self._open_collection()
-    while not self.terminator.is_set() or not self.input.empty():
-      item = self.input.get()
-      self.output.write(json.dumps(item) + ',\n')
-    self._close_collection()
+    def run(self):
+        self._open_collection()
+        while not self.terminator.is_set() or not self.input.empty():
+            item = self.input.get()
+            self.output.write(json.dumps(item) + ",\n")
+        self._close_collection()
 
 
 class TraceProfiler(object):
-  """A python trace profiler that outputs Chrome Trace-Viewer format (about://tracing).
+    """A python trace profiler that outputs Chrome Trace-Viewer format (about://tracing).
 
-     Usage:
+    Usage:
 
-        from pytracing import TraceProfiler
-        tp = TraceProfiler(output=open('/tmp/trace.out', 'wb'))
-        with tp.traced():
-          ...
+       from pytracing import TraceProfiler
+       tp = TraceProfiler(output=open('/tmp/trace.out', 'wb'))
+       with tp.traced():
+         ...
 
-  """
-  TYPES = {'call': 'B', 'return': 'E'}
+    """
 
-  def __init__(self, output, clock=None):
-    self.output = output
-    self.clock = clock or time.time
-    self.pid = os.getpid()
-    self.queue = Queue()
-    self.terminator = threading.Event()
-    self.writer = TraceWriter(self.terminator, self.queue, self.output)
+    TYPES = {"call": "B", "return": "E"}
 
-  @property
-  def thread_id(self):
-    return threading.current_thread().name
+    def __init__(self, output, clock=None):
+        self.output = output
+        self.clock = clock or time.time
+        self.pid = os.getpid()
+        self.queue = Queue()
+        self.terminator = threading.Event()
+        self.writer = TraceWriter(self.terminator, self.queue, self.output)
 
-  @contextmanager
-  def traced(self):
-    """Context manager for install/shutdown in a with block."""
-    self.install()
-    try:
-      yield
-    finally:
-      self.shutdown()
+    @property
+    def thread_id(self):
+        return threading.current_thread().name
 
-  def install(self):
-    """Install the trace function and open the JSON output stream."""
-    self.writer.start()               # Start the writer thread.
-    sys.setprofile(self.tracer)        # Set the trace/profile function.
-    threading.setprofile(self.tracer)  # Set the trace/profile function for threads.
+    @contextmanager
+    def traced(self):
+        """Context manager for install/shutdown in a with block."""
+        self.install()
+        try:
+            yield
+        finally:
+            self.shutdown()
 
-  def shutdown(self):
-    sys.setprofile(None)                # Clear the trace/profile function.
-    threading.setprofile(None)          # Clear the trace/profile function for threads.
-    self.terminator.set()              # Stop the writer thread.
-    self.writer.join()                 # Join the writer thread.
+    def install(self):
+        """Install the trace function and open the JSON output stream."""
+        self.writer.start()  # Start the writer thread.
+        sys.setprofile(self.tracer)  # Set the trace/profile function.
+        threading.setprofile(self.tracer)  # Set the trace/profile function for threads.
 
-  def fire_event(self, event_type, func_name, func_filename, func_line_no,
-                 caller_filename, caller_line_no):
-    """Write a trace event to the output stream."""
-    timestamp = to_microseconds(self.clock())
-    # https://docs.google.com/document/d/1CvAClvFfyA5R-PhYUmn5OOQtYMH4h6I0nSsKchNAySU/preview
+    def shutdown(self):
+        sys.setprofile(None)  # Clear the trace/profile function.
+        threading.setprofile(None)  # Clear the trace/profile function for threads.
+        self.terminator.set()  # Stop the writer thread.
+        self.writer.join()  # Join the writer thread.
 
-    event = dict(
-      name=func_name,                 # Event Name.
-      cat=func_filename,               # Event Category.
-      tid=self.thread_id,             # Thread ID.
-      ph=self.TYPES[event_type],      # Event Type.
-      pid=self.pid,                   # Process ID.
-      ts=timestamp,                   # Timestamp.
-      args=dict(
-        function=':'.join([str(x) for x in (func_filename, func_line_no, func_name)]),
-        caller=':'.join([str(x) for x in (caller_filename, caller_line_no)]),
-      )
-    )
-    self.queue.put(event)
+    def fire_event(self, event_type, func_name, func_filename, func_line_no, caller_filename, caller_line_no):
+        """Write a trace event to the output stream."""
+        timestamp = to_microseconds(self.clock())
+        # https://docs.google.com/document/d/1CvAClvFfyA5R-PhYUmn5OOQtYMH4h6I0nSsKchNAySU/preview
 
-  def tracer(self, frame, event_type, arg):
-    """Bound tracer function for sys.settrace()."""
-    try:
-      if event_type in self.TYPES.keys() and frame.f_code.co_name != 'write':
-        self.fire_event(
-          event_type=event_type,
-          func_name=frame.f_code.co_name,
-          func_filename=frame.f_code.co_filename,
-          func_line_no=frame.f_lineno,
-          caller_filename=frame.f_back.f_code.co_filename,
-          caller_line_no=frame.f_back.f_lineno,
+        event = dict(
+            name=func_name,  # Event Name.
+            cat=func_filename,  # Event Category.
+            tid=self.thread_id,  # Thread ID.
+            ph=self.TYPES[event_type],  # Event Type.
+            pid=self.pid,  # Process ID.
+            ts=timestamp,  # Timestamp.
+            args=dict(
+                function=":".join([str(x) for x in (func_filename, func_line_no, func_name)]),
+                caller=":".join([str(x) for x in (caller_filename, caller_line_no)]),
+            ),
         )
-    except Exception:
-      pass  # Don't disturb execution if we can't log the trace.
+        self.queue.put(event)
+
+    def tracer(self, frame, event_type, arg):
+        """Bound tracer function for sys.settrace()."""
+        try:
+            if event_type in self.TYPES.keys() and frame.f_code.co_name != "write":
+                self.fire_event(
+                    event_type=event_type,
+                    func_name=frame.f_code.co_name,
+                    func_filename=frame.f_code.co_filename,
+                    func_line_no=frame.f_lineno,
+                    caller_filename=frame.f_back.f_code.co_filename,
+                    caller_line_no=frame.f_back.f_lineno,
+                )
+        except Exception:
+            pass  # Don't disturb execution if we can't log the trace.
--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@
 from setuptools import setup
 
 setup(
-  name='pytracing',
-  version='0.4',
-  description='a python trace profiler that outputs to chrome trace-viewer format (about://tracing).',
-  author='Kris Wilson',
-  author_email='kwilson@twitter.com',
-  url='https://www.github.com/kwlzn/pytracing',
-  packages=['pytracing']
+    name="pytracing",
+    version="0.4",
+    description="a python trace profiler that outputs to chrome trace-viewer format (about://tracing).",
+    author="Kris Wilson",
+    author_email="kwilson@twitter.com",
+    extras_require={"test": ["flake8==3.9.2", "pre-commit"]},
+    url="https://www.github.com/kwlzn/pytracing",
+    packages=["pytracing"],
 )
